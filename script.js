40222f1d8a2c962302bef3ba81d6d912867776f3
Mark Leybourne <mark.leybourne@sage.com>
2022-12-05 10:13:23 +0000

    Further implementation

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { Trend } from 'k6/metrics';
 
-export default function () {
+// 1. init context code
+// - example: global variables
+const customMetric = new Trend('customMetricExample');
+
+export function setup() {
+  // 2. setup code
+  return { name: "Example data object" };
+}
+
+export default function (data) {
+  // 3. VU code
+  console.log(JSON.stringify(data));
   http.get('https://test.k6.io');
   sleep(1);
+  customTrend.add(1);
+}
+
+export function teardown(data) {
+  // 4. teardown code
+  console.log(JSON.stringify(data));
 }
